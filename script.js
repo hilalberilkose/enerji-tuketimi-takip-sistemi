4b3192ab5dee1f6b501ea108905de633bda8e5ff
hilalberilkose <hilalberilkose@gmail.com>
2026-06-03 10:19:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 const ctx = document.getElementById('enerjiGrafik');
 
 new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
         labels: ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran'],
         datasets: [{
             label: 'Enerji Tüketimi (kWh)',
-            data: [820, 760, 910, 700, 640, 780]
+            data: [820, 760, 910, 700, 640, 780],
+            tension: 0.4
         }]
     },
     options: {
-        responsive: true,
-        plugins: {
-            legend: {
-                display: true
-            }
-        }
+        responsive: true
     }
 });
