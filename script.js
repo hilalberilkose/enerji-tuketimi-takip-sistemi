3ff2111f14818df5593bbe81d32c3bc4a4883aad
hilalberilkose <hilalberilkose@gmail.com>
2026-06-03 10:26:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,48 @@ new Chart(ctx, {
         responsive: true
     }
 });
+
+const maliyetCtx = document.getElementById('maliyetGrafik');
+
+new Chart(maliyetCtx, {
+    type: 'bar',
+    data: {
+        labels: ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran'],
+        datasets: [{
+            label: 'Enerji Maliyeti (TL)',
+            data: [2500, 2300, 2900, 2100, 1900, 2400]
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
+
+document.getElementById('temaButonu')
+    .addEventListener('click', function () {
+        document.body.classList.toggle('dark-mode');
+    });
+
+function hesapla() {
+
+    let tuketim = parseFloat(
+        document.getElementById('gunlukTuketim').value
+    );
+
+    let fiyat = parseFloat(
+        document.getElementById('birimFiyat').value
+    );
+
+    if (isNaN(tuketim) || isNaN(fiyat)) {
+        document.getElementById('sonuc').innerHTML =
+            'Lütfen tüm alanları doldurun';
+        return;
+    }
+
+    let aylik = tuketimfiyat30;
+
+    document.getElementById('sonuc').innerHTML =
+        'Aylık Tahmini Maliyet: ' +
+        aylik.toFixed(2) +
+        ' TL';
+}
